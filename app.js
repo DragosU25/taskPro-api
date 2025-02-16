ef34a735bec0c3ea1c75ce2714f5041d75cdad05
Dragos Ungureanu <ungureanud6@gmail.com>
2025-02-16 16:49:17 +0100

    update

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./src/config/db');
 const authRoutes = require('./src/routes/auth');
 const projectRoutes = require('./src/routes/project');
-const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
 const swaggerUi = require('swagger-ui-express');
 const specs = require('./swagger');
-
-dotenv.config();
-
-const app = express();
-
-app.use('/avatars', express.static(path.join(__dirname, 'public/avatars')));
-
 const sgMail = require('@sendgrid/mail');
-require('dotenv').config();
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-});
+const app = express();
 
+// Middleware pentru fișiere statice
+app.use('/avatars', express.static(path.join(__dirname, 'public/avatars')));
+
+// Setare SendGrid
 const sendGridApiKey = process.env.SENDGRID_TOKEN;
-if (!sendGridApiKey) throw new Error('SendGrid API key is missing.');
-
+if (!sendGridApiKey) {
+  console.error('SendGrid API key is missing.');
+  process.exit(1);
+}
 sgMail.setApiKey(sendGridApiKey);
 
-//connect to database
+// Conectare la baza de date
 connectDB();
 
-// Enable CORS for all origins
-app.use(cors());
+// CORS
+app.use(
+  cors({
+    origin: ['https://taskpro-frontend.onrender.com', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
-//middleware
+// Middleware-uri globale
 app.use(express.json());
-
-// Apply rate limiter to all requests
-app.use(limiter);
-
-// Use Helmet
 app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 
-//swagger
+// Swagger API docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-//Routes
+// Rute
 app.use('/api/auth', authRoutes);
 app.use('/api/project', projectRoutes);
-const PORT = process.env.PORT || 'https://taskpro-api-ca4u.onrender.com';
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Pornire server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
